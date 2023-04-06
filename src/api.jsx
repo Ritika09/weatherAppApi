2b22bcc0efef35856a57d5d22df2393da4f5d613
Ritika09 <ritikamamta88@gmail.com>
2023-04-05 22:57:43 -0700

    second commit

---
+++ src/api.jsx
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "ceda99ba49msh8bede191ea2a396p153255jsnff54fcd778dc",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "82931d0ace7a536499573168b269f589"; // enter your key from openweather API
